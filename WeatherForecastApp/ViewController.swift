a5524ad863dc1e11171ebc57cb80563889b874f9
Tatyana Tepaeva <ap.nnn1@gmail.com>
2017-10-18 23:25:19 +0300

    print geodata to console for test purposes

--- WeatherForecastApp/ViewController.swift
+++ WeatherForecastApp/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    print("Start!")
+
+    LocationService.instance.authorized.asObservable()
+      .subscribe(onNext: { authorized in print(authorized) })
+      .disposed(by: disposeBag)
+
+    LocationService.instance.location.asObservable()
+      .subscribe(onNext: { location in print(location) })
+      .disposed(by: disposeBag)
+
+    let geoCoderService = GeoCoderService()
+    LocationService.instance.location.asObservable()
+      .flatMapLatest { geoCoderService.getCityName($0) }
+      .subscribe(onNext: { city in print(city) })
+      .disposed(by: disposeBag)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 }
